6398eb6c7fc0f912d72a50eafc60b3d22cc90450
Tuan30 <anhtuan28023008@gmail.com>
2023-04-01 10:14:04 +0700

    commit 1/4

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const port = 3000
+const morgan = require('morgan')
+const app = express()
+const handlebars = require('express-handlebars')
+
+//HTTP Logger
+app.use(morgan('combined'))
+
+//Tamplate engine
+app.engine('hbs', handlebars.engine({ extname: '.hbs', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
